a773b60b777a9d866979dff70c6c99fb2497aaf7
Andreas Martiny <andreasmartiny@Andreass-Air.fritz.box>
2022-12-02 22:35:57 +0100

    random changes

--- src/App.js
+++ src/App.js
@@ -1,91 +1,100 @@
-import {useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-// State
+  // State
 
-const [teams, setTeams] = useState([]);
-const [games, setGames] = useState([]);
-const [criteria, setCriteria] = useState({
-  startDate: "2022-11-19",
-  endDate: "2022-11-29",
-  teamId: 1
-});
+  const [teams, setTeams] = useState([]);
+  const [games, setGames] = useState([]);
+  const [criteria, setCriteria] = useState({
+    startDate: "2022-11-19",
+    endDate: "2022-11-29",
+    teamId: null
+  });
 
-// fetch all teams & set dropdown state
+  // fetch all teams & set dropdown state
 
-useEffect( () => {
-  const fetchData = async () => {
-  const res = await fetch(`https://www.balldontlie.io/api/v1/teams`);
-  const data = await res.json();
-  setTeams(data.data);
-  }
-  fetchData();
-}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`https://www.balldontlie.io/api/v1/teams`);
+      const data = await res.json();
+      setTeams(data.data);
+    }
+    fetchData();
+  }, []);
 
-// Handle change in startDateSelector & set StartDate
+  // Handle change in startDateSelector & set StartDate
 
-const handleStartDateChange = (event) => {
-  setCriteria({
-    ...criteria,
-    startDate: event.target.value,
-})};
-
-// Handle change in endDateSelector & set endDate
-
-const handleEndDateChange = (event) => {
-  setCriteria({
-    ...criteria,
-    endDate: event.target.value
-  })};
-
-// Handle change in TeamSelector & setTeam
-
-const handleSelectedTeamChange = (event) => {
-  setCriteria({
-    ...criteria,
-    teamId: event.target.value
-  })};
-
-// fetch games, sort by date ascending and set games state
-
-const fetchGames = async () => {
-  const url = `https://www.balldontlie.io/api/v1/games?start_date=${criteria.startDate}&end_date=${criteria.endDate}&team_ids[]=${criteria.teamId}`;
-  const res = await fetch (url);
-  const data = await res.json();
-  setGames(data.data.sort((a, b) => new Date(a.date) - new Date(b.date)));
+  const handleStartDateChange = (event) => {
+    setCriteria({
+      ...criteria,
+      startDate: event.target.value,
+    })
   };
 
-  return (
-  <div className='flex justify-center'>
-  
-    <div className='w-1/2'> 
+  // Handle change in endDateSelector & set endDate
 
-      <div className=' border-2 rounded-md m-4 p-4'>
+  const handleEndDateChange = (event) => {
+    setCriteria({
+      ...criteria,
+      endDate: event.target.value
+    })
+  };
+
+  // Handle change in TeamSelector & setTeam
+
+  const handleSelectedTeamChange = (event) => {
+    setCriteria({
+      ...criteria,
+      teamId: event.target.value
+    })
+  };
+
+  // fetch games, sort by date ascending and set games state
+
+  const fetchGames = async () => {
+    const url = `https://www.balldontlie.io/api/v1/games?start_date=${criteria.startDate}&end_date=${criteria.endDate}&team_ids[]=${criteria.teamId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setGames(data.data.sort((a, b) => new Date(a.date) - new Date(b.date)));
+  };
+
+  const handleResetButtonClick = () => {
+    setGames([]);
+  }
+
+  return (
+    <div className=' justify-center m-0'>
+
+      <div className=''>
+
+      <div className='border-2 rounded-md m-4 p-4'>
 
         <div className='m-4 border-2 flex justify-center'>
-          <label className='mr-4' htmlFor="teamSelector">Team:</label>
-          <select className='border-2 rounded-md' name="teamSelector" id="teamSelector" onChange={handleSelectedTeamChange}>
+          <select className=' border-2 rounded-md' name="teamSelector" id="teamSelector" onChange={handleSelectedTeamChange}>
+          <option value="" disabled selected hidden>Pick a team!</option>
             {teams.map(team => <option value={team.id} key={team.id}>{team.full_name}</option>)}
           </select>
           <label htmlFor="teamSelector"></label>
         </div>
 
+
         <div className='m-4 border-2 flex justify-center'>
-          <input className='border-2 rounded-md' type="date" id="startDateSelector" name="startDateSelector" onChange={handleStartDateChange}></input>
-        <span className='m-4'>To</span>
-          <input className='border-2 rounded-md' type="date" id="endDateSelector" name="endDateSelector" onChange={handleEndDateChange}></input>
+          <input className=' border-2 rounded-md ::-webkit-calendar-picker-indicator' type="date" id="startDateSelector" name="startDateSelector" onChange={handleStartDateChange}></input>
+          <span className='m-4'>To</span>
+          <input className=' border-2 rounded-md' type="date" id="endDateSelector" name="endDateSelector" onChange={handleEndDateChange}></input>
         </div>
 
         <div className='m-4 flex justify-center'>
-          <button className='border-2 rounded-md p-1' onClick={fetchGames} disabled={!criteria.endDate || !criteria.startDate || !criteria.teamId}>Fetch games</button>
+          <button class='hover:bg-secondary rounded-md p-1 mx-1 border-secondary bg-gray-300 disabled:bg-gray-300 transition ease-in-out' onClick={fetchGames} disabled={!criteria.endDate || !criteria.startDate || !criteria.teamId}>{!criteria.endDate || !criteria.startDate || !criteria.teamId ? "Fill in search criteria!" : "Search"}</button>
+          { games.length !== 0 ? <button class='bg-gray-300 rounded-md p-1 mx-1 hover:bg-secondary hover:shadow-sm transition ease-in-out' onClick={handleResetButtonClick}>Reset</button> : null }
         </div>
-    
+
 
       </div>
 
       <div className=''>{games.map(game =>
-        <div className='bg-white border-2 rounded-md m-4' key={game.id}>
+        <div className=' border-2 rounded-md m-4' key={game.id}>
           <div className='flex justify-center'>{game.date.slice(0, 10)} @ {game.status}</div>
           <div className='flex justify-center'>{game.home_team.abbreviation} - {game.visitor_team.abbreviation}</div>
           <div className='flex justify-center'>{game.time === "" ? "" : game.home_team_score + " - " + game.visitor_team_score}</div>
@@ -93,9 +102,9 @@ const fetchGames = async () => {
 
       </div>
 
-    </div>
+      </div>
 
-  </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#1e2124',
+        'secondary': '#7289da',
+        'tertiary': '#404EED'
+      },
+    },
   },
   plugins: [],
 }
